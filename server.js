f7a3ac73eca3a78857e874b7125348efa767279c
mwas999-ux <freddiemwas999@gmail.com>
2026-04-29 13:21:14 +0300

    fixed forgot password function scope bug

--- server.js
+++ server.js
@@ -56,12 +56,15 @@ pool.query(`
 
 // ── EMAIL SETUP ──
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
-});
+});;
 
 // ── JWT MIDDLEWARE ──
 function authMiddleware(req, res, next) {
